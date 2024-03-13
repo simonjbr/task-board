e496ebd1204a696b88a039f473dd9c3babed92ad
Simon Brown <simonandon@live.com.au>
2024-03-13 13:32:12 +0800

    Wrote generateTaskId() and createTaskCard() functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,41 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+	return crypto.randomUUID();
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+	// create all elements for the task card
+	const $taskCardEl = $('<div>').addClass("card project-card draggable my-3").attr("data-task-id", task.id);
+	const $taskTitleEl = $('<h4>').addClass("card-header h4").text(task.name);
+	const $taskBodyEl = $('<div>').addClass("card-body");
+	const $taskDescEl = $('<p>').addClass("card-text").text(task.description);
+	const $taskDueDateEl = $('<p>').addClass("card-text").text(task.dueDate);
+	const $taskDeleteBtnEl = $('<button>').addClass("btn btn-danger delete btn-delete-project").text("delete").attr("data-task-id", task.id);
 
+	// set background colour based on the due date
+	if (task.dueDate && task.status !== 'done') {
+		// establish current date and task dueDate
+		const now = dayjs();
+		const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+
+		// adjust background colour for tasks due today or overdue
+		if (now.isSame(taskDueDate, 'day')) {
+			$taskCardEl.addClass("bg-warning text-white");
+		} else if (now.isAfter(taskDueDate)) {
+			$taskCardEl.addClass("bg-danger text-white");
+			$taskDeleteBtnEl.addClass("border-light");
+		}
+	}
+
+	// append description due date and delete button to task body element
+	$taskBodyEl.append($taskDescEl, $taskDueDateEl, $taskDeleteBtnEl);
+	// append title and body to the task card
+	$taskCardEl.append($taskTitleEl, $taskBodyEl);
+
+	// return fully formed task card
+	return $taskCardEl;
 }
 
 // Todo: create a function to render the task list and make cards draggable
